e4074acbd1c712edf1714422b57a3ef1e86faf23
bidurchettri <cvidurchettri@gmail.com>
2021-08-01 13:41:50 +0530

    Add files via upload

--- select.php
+++ select.php
@@ -1,66 +1,63 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>students</title>
-	<style>
-#customers {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+	<title>Students</title>
+  <link href="main.css" rel="stylesheet"></style>
+<style>
 
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#customers tr:nth-child(even){background-color: #f2f2f2;}
-
-#customers tr:hover {background-color: #ddd;}
-
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #04AA6D;
-  color: white;
-}
 </style>
 </head>
 <body>
-<h1><center>Students</center></h1>
+
+
+<div style="background-color: blue;color:white; padding-top: 20px;padding-bottom: 20px;">
+  <h1><center>Search Students</center></h1>
+  <form action="select.php" method="POST">
+    <center> 
+      <input type="text" name="Rollno" placeholder="Enter RollNo" class="in">
+      <input type="submit" name="search" value="Serach" class="bt">
+    </center>
+  </form>
+</div>
 <hr>
+
+
+
+
+
 
 <?php
 include"connection.php";
-$sql = "SELECT * FROM students_info;";
-$result = $conn->query($sql);
- 
-
-if ($result->num_rows > 0) 
+if(isset($_POST["search"]))
 {
+  $rollno=$_POST["Rollno"];
+  $sql = "SELECT * FROM students_info WHERE rollno='$rollno';";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) 
+  {
     // output data of each row
     echo"<table id='customers'>
     <tr>
     <td>Name</td>
     <td>Roll no</td>
-    <td> sem</td></tr>";
+    <td> Sem</td></tr>";
     while($row = $result->fetch_assoc()) 
     {
-        echo "<tr>
-              <td>" . $row["name"]. "</td>".
-              "<td>" . $row["rollno"]. "</td>".
-              "<td>" . $row["sem"]. "</td>"
-              ."</tr>";
-    }
+      echo "<tr>
+      <td>" . $row["name"]. "</td>".
+      "<td>" . $row["rollno"]. "</td>".
+      "<td>" . $row["sem"]. "</td>"
+      ."</tr>";
+     }
     echo"</td>";
-
-} 
-else 
-{
+  } 
+  else 
+  {
     echo "0 results";
+  }
 }
-
 
 $conn->close();
 ?>
+
+
